3d7cad054a23ff7631c661c1dcd728f2dbb779f4
m7mdy9 <hamestar1122@gmail.com>
2025-04-09 16:33:36 +0200

    k

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const json = require("./index.json")
-const ht = require("./index.html")
+// const json = require("./index.json")
+// const ht = require("./index.html")
 
-var hi = json.x1
-console.log(hi);
+// var hi = json.x1
+console.log(`hi`);
 function func1(){
     document.getElementById("1234").style.display = "block";
     document.getElementById("1234").style.textAlign = "center";
     alert("Here you go a cool cat picture!");
-    }
+}
